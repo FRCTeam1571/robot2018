dd1e5bf14038f28c5229f2c478b061426239bf25
FRC1571-Calibrate <calrobotics1571@gmail.com>
2018-02-10 13:05:46 -0800

    After fix for button toggle
    - Commented out test for buttons
    - Commented out test for moter control with bumpers

--- robot.py
+++ robot.py
@@ -2,10 +2,9 @@
 #!/usr/bin/env python3
 
 import wpilib
-from wpilib import drive
-from wpilib import timer
+from wpilib import drive, timer
 import ctre
-# from networktables import networktables
+# from networktables import NetworkTables
 
 
 class robot(wpilib.IterativeRobot):
@@ -24,6 +23,9 @@ class robot(wpilib.IterativeRobot):
         self.rl_motor = ctre.wpi_talonsrx.WPI_TalonSRX(1)
         self.left = wpilib.SpeedControllerGroup(self.fl_motor, self.rl_motor)
 
+        self.lift = wpilib.Solenoid(0, 0)
+        self.grab = wpilib.Solenoid(0, 1)
+
 
         # self.rr_motor.set(mode(Follower))
         # self.rr_motor.set(self.fr_motor.getDeviceID())
@@ -36,45 +38,94 @@ class robot(wpilib.IterativeRobot):
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
-        wpilib.timer.reset()
-        wpilib.timer.start()
+        self.wpilib.Timer.reset()
+        self.wpilib.Timer.start()
 
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
         while self.isAutonomous() and self.isEnabled():
             # Drive for two seconds
-            if self.timer.get() < 2.0:
+            if self.Timer.get() < 2.0:
                 self.drive.arcadeDrive(-0.5, 0)  # Drive forwards at half speed
             else:
                 self.drive.arcadeDrive(0, 0)  # Stop robot
 
     def teleopInit(self):
         """This function is run once each time the robot enters telepo mode"""
-        self.TriggerLeft = self.controller.getTriggerAxis()
         self.kLeft = self.controller.Hand.kLeft
         self.kRight = self.controller.Hand.kRight
+        self.lift.set(False)
+        self.grab.set(False)
+        self.LiftToggle = False
+        self.GrabToggle = False
+        self.GrabLast = False
+
+
+
 
     def teleopPeriodic(self):
+        # self.lock = 0
+        # def axisoutput(numx, numy):
+        #     if self.lock == 0:
+        #         self.lock = 1
+        #         print(numx, numy)
+        #         time.sleep(2.5)
+        #         self.lock = 0
+        #     return
         """This function is called periodically during operator control."""
-        # NOTE: For Deploy
         while self.isOperatorControl() and self.isEnabled():
 
+            self.TriggerLeft = self.controller.getTriggerAxis(self.kLeft)
+            self.TriggerRight = self.controller.getTriggerAxis(self.kRight)
+            self.BumperLeft = self.controller.getBumper(self.kLeft)
+            self.BumperRight = self.controller.getBumper(self.kRight)
+            # self.lAxis = self.controller.getRawAxis(2)
+            # self.rAxis = self.controller.getRawAxis(3)
+            # # print(self.controller.getMagnitude())
+            # threads = []
+            # t = threading.Thread(target=axisoutput, args=(self.lAxis, self.rAxis))
+            # threads.append(t)
+            # t.start()
+
             # backwards control
-            if self.controller.getBumper(self.kLeft):
+            if self.BumperLeft:
                 self.TriggerLeft = self.TriggerLeft * -1
 
-            # if self.controller.getTriggerAxis(self.kLeft) < 0:
-            #     self.TriggerLeft = 0
-            # elif self.controller.getTriggerAxis(self.kRight) > 0:
-            #     self.TriggerLeft = 0
 
-            if self.controller.getBumper(self.kRight):
+            if self.BumperRight:
                 self.TriggerRight = self.TriggerRight * -1
 
-
+            # Drive
             self.drive.arcadeDrive(self.TriggerLeft, self.controller.getX(self.kLeft))
-        # NOTE: For Simulator
-        # self.drive.tankDrive(self.controller.getRawAxis(1), self.controller.getRawAxis(3))
+
+            # Solenoids
+
+            if self.controller.getAButtonPressed() and self.lift.get() == False:
+                self.lift.set(True)
+            elif self.controller.getAButtonReleased() and self.lift.get() == True:
+                self.lift.set(False)
+
+            if self.controller.getXButton() and not self.GrabLast:
+                self.GrabToggle = not self.GrabToggle
+                # self.GrabLast = True
+            # elif self.controller.getXButton() and self.GrabToggle and self.GrabLast == True:
+            #     # self.grab.set(False)
+            #     self.GrabToggle = False
+            #     self.GrabLast = False
+
+            self.GrabLast = self.controller.getXButton()
+
+
+            if self.GrabToggle and self.GrabLast:
+                self.grab.set(True)
+            elif self.GrabLast:
+                self.grab.set(False)
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
