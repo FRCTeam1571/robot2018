1223d63f405d66217a99123acf274b042c2bd2e4
FRC1571-Calibrate <calrobotics1571@gmail.com>
2018-02-17 10:33:02 -0800

    removed some stuff

--- robot.py
+++ robot.py
@@ -2,7 +2,7 @@
 #!/usr/bin/env python3
 
 import wpilib
-from wpilib import drive, timer
+from wpilib import drive, Timer
 import ctre
 # from networktables import NetworkTables
 
@@ -27,19 +27,16 @@ class robot(wpilib.IterativeRobot):
         self.grab = wpilib.Solenoid(0, 1)
 
 
-        # self.rr_motor.set(mode(Follower))
-        # self.rr_motor.set(self.fr_motor.getDeviceID())
-        # self.rl_motor.set(mode(Follower))
-        # self.rl_motor.set(self.fl_motor.getDeviceID())
-
 
 
         self.drive = wpilib.drive.DifferentialDrive(self.left, self.right)
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
-        self.wpilib.Timer.reset()
-        self.wpilib.Timer.start()
+        self.timer = wpilib.Timer
+
+        self.timer.start()
+        self.timer.reset()
 
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
@@ -56,7 +53,7 @@ class robot(wpilib.IterativeRobot):
         self.kRight = self.controller.Hand.kRight
         self.lift.set(False)
         self.grab.set(False)
-        self.LiftToggle = False
+        # self.LiftToggle = False
         self.GrabToggle = False
         self.GrabLast = False
 
@@ -64,14 +61,7 @@ class robot(wpilib.IterativeRobot):
 
 
     def teleopPeriodic(self):
-        # self.lock = 0
-        # def axisoutput(numx, numy):
-        #     if self.lock == 0:
-        #         self.lock = 1
-        #         print(numx, numy)
-        #         time.sleep(2.5)
-        #         self.lock = 0
-        #     return
+
         """This function is called periodically during operator control."""
         while self.isOperatorControl() and self.isEnabled():
 
@@ -79,13 +69,7 @@ class robot(wpilib.IterativeRobot):
             self.TriggerRight = self.controller.getTriggerAxis(self.kRight)
             self.BumperLeft = self.controller.getBumper(self.kLeft)
             self.BumperRight = self.controller.getBumper(self.kRight)
-            # self.lAxis = self.controller.getRawAxis(2)
-            # self.rAxis = self.controller.getRawAxis(3)
-            # # print(self.controller.getMagnitude())
-            # threads = []
-            # t = threading.Thread(target=axisoutput, args=(self.lAxis, self.rAxis))
-            # threads.append(t)
-            # t.start()
+
 
             # backwards control
             if self.BumperLeft:
@@ -107,11 +91,7 @@ class robot(wpilib.IterativeRobot):
 
             if self.controller.getXButton() and not self.GrabLast:
                 self.GrabToggle = not self.GrabToggle
-                # self.GrabLast = True
-            # elif self.controller.getXButton() and self.GrabToggle and self.GrabLast == True:
-            #     # self.grab.set(False)
-            #     self.GrabToggle = False
-            #     self.GrabLast = False
+
 
             self.GrabLast = self.controller.getXButton()
 
