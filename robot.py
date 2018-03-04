c3343db95debd537bc8efe1c00e2b631efcada99
FRCTeam1571-Programmer <calrobotics1571@gmail.com>
2018-03-03 16:04:10 -0800

    Last Commit of clackamas comp
    A bunch of stuff that includes:
    - Added support for two cameras
    - Added support for Beshoy's thing
    - Middle wheel code is commented out
    - Started working on NetworkTables stuff

--- robot.py
+++ robot.py
@@ -7,10 +7,6 @@ from wpilib import drive, Timer
 import ctre
 from networktables import NetworkTables
 
-#import cscore
-
-#import cv2
-import numpy as np
 
 
 
@@ -19,7 +15,15 @@ class robot(wpilib.IterativeRobot):
     def robotInit(self):
         '''Robot Initiation'''
 
-        # NetworkTables.initialize(server='roborio-1571-frc.local')
+        NetworkTables.initialize(server='roborio-1571-frc.local')
+        self.sd = NetworkTables.getTable('SmartDashboard')
+
+
+
+        # wpilib.CameraServer.launch()
+        wpilib.CameraServer.launch('vision.py:main')
+
+
 
 
 
@@ -36,15 +40,19 @@ class robot(wpilib.IterativeRobot):
         self.rl_motor = ctre.wpi_talonsrx.WPI_TalonSRX(1) #1
         self.left = wpilib.SpeedControllerGroup(self.fl_motor, self.rl_motor)
 
-        # Middle motor
+        # Middle motor ###UNUSED###
         self.mid_motor = ctre.wpi_talonsrx.WPI_TalonSRX(5)
 
 
         # Talon SR #
         # Grab motors
-        self.intake_r = wpilib.Talon(0)
-        self.intake_l = wpilib.Talon(1)
+        self.intake_r = wpilib.Talon(0) #0
+        self.intake_l = wpilib.Talon(1) #1
         self.intake = wpilib.SpeedControllerGroup(self.intake_r, self.intake_l)
+
+        # Relay #
+        # Loader
+        self.loader = wpilib.Relay(1) #0
 
         # Solenoids #
         self.lift = wpilib.Solenoid(0, 0)
@@ -53,6 +61,7 @@ class robot(wpilib.IterativeRobot):
         self.timer = wpilib.Timer()
 
         self.drive = wpilib.drive.DifferentialDrive(self.left, self.right)
+
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
@@ -64,15 +73,15 @@ class robot(wpilib.IterativeRobot):
         """This function is called periodically during autonomous."""
         while self.isAutonomous() and self.isEnabled():
             # Drive for two seconds
-            if self.timer.get() < 0.0:
-                self.drive.arcadeDrive(-0.5, 0)  # Drive forwards at half speed
-            elif self.timer.get() < 5.0:
-                self.drive.arcadeDrive(0, 0)
+            if self.timer.get() > 0.0 and self.timer.get() < 10.0:
+                self.drive.arcadeDrive(0.5, 0)  # Drive forwards at half speed
+            # elif self.timer.get() == 5.0:
+            #     self.drive.arcadeDrive(0, 0)
             else:
                 self.drive.arcadeDrive(0, 0)  # Stop robot
 
     def teleopInit(self):
-        """This function is run once each time the robot enters telepo mode"""
+        """This function is run once each time the robot enters teleop mode"""
         self.kLeft = self.controller.Hand.kLeft
         self.kRight = self.controller.Hand.kRight
         self.lift.set(False)
@@ -99,17 +108,22 @@ class robot(wpilib.IterativeRobot):
             if self.BumperLeft:
                 self.TriggerLeft = self.TriggerLeft * -1
 
-
             if self.BumperRight:
                 self.TriggerRight = self.TriggerRight * -1
 
             # Drive #
             self.drive.arcadeDrive(self.TriggerLeft, self.controller.getX(self.kLeft))
 
-            # Middle wheel
+            # Middle wheel #
+            # trigger control
             # self.mid_motor.set(self.TriggerRight)
-            self.mid_motor.set(self.controller.getX(self.kRight))
+            # stick control
+            # if abs(self.controller.getX(self.kRight)) > 0.2:
+            #     self.mid_motor.set(self.controller.getX(self.kRight))
+            # else:
+            #     self.mid_motor.set(0.0)
 
+            # Intake Motors #
             if self.controller.getBButton():
                 self.intake.set(0.25)
             elif self.controller.getYButton():
@@ -117,26 +131,38 @@ class robot(wpilib.IterativeRobot):
             else:
                 self.intake.set(0)
 
-
-
-
             # Solenoids #
-
-            if self.controller.getAButtonPressed() and self.lift.get() == False:
+            # Lift
+            if self.controller.getAButtonPressed() and not self.lift.get():
                 self.lift.set(True)
-            elif self.controller.getAButtonReleased() and self.lift.get() == True:
+            elif self.controller.getAButtonReleased() and self.lift.get():
                 self.lift.set(False)
 
+            # Grabber
             if self.controller.getXButton() and not self.GrabLast:
-                self.GrabToggle = not self.GrabToggle
+                 self.GrabToggle = not self.GrabToggle
 
             self.GrabLast = self.controller.getXButton()
 
+            # Loader #
+            # Control
+            if self.controller.getRawButton(9):
+                self.loader.set(wpilib.Relay.Value.kForward)
+            elif self.controller.getRawButton(10):
+                self.loader.set(wpilib.Relay.Value.kReverse)
+            else:
+                self.loader.set(wpilib.Relay.Value.kOff)
 
-            if self.GrabToggle and self.GrabLast:
-                self.grab.set(True)
-            elif self.GrabLast:
-                self.grab.set(False)
+            # SmartDashboard
+            self.relayvalue = self.loader.get()
+            if self.relayvalue == wpilib.Relay.Value.kForward:
+                self.relaybool = True
+            else:
+                self.relaybool = False
+
+            self.sd.putNumber('Relay Value', self.loader.get())
+            self.sd.putBoolean('Relay Fwd', self.relaybool)
+
 
 
 
